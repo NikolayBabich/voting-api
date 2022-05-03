e1f58fa21136b14f6786192da9bd05a53c41e8a7
Nikolay Babich <68919737+NikolayBabich@users.noreply.github.com>
2022-05-03 21:49:02 +0300

    Restart from scratch

--- src/main/java/com/github/nikolaybabich/voting/RestaurantVotingApplication.java
+++ src/main/java/com/github/nikolaybabich/voting/RestaurantVotingApplication.java
@@ -1,4 +1,4 @@
-package ru.javaops.voting;
+package com.github.nikolaybabich.voting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
